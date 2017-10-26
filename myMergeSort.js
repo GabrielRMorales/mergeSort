26a6962401cd309bb55bca1791e514a737cdffe0
Gabriel Morales <grmorale@gmail.com>
2017-10-26 14:30:18 -0700

    Minorly refactor code for clarity

--- myMergeSort.js
+++ myMergeSort.js
@@ -3,7 +3,7 @@ function mergeSort(arr){
   return [arr[0]];
   }
   
-  return mer(sl(arr.slice(0, arr.length/2)),sr(arr.slice(arr.length/2, arr.length)));
+  return merge(sl(arr.slice(0, arr.length/2)),sr(arr.slice(arr.length/2, arr.length)));
   
   function sl(arr){
     return mergeSort(arr);
@@ -13,30 +13,21 @@ function mergeSort(arr){
     return mergeSort(arr);
   }
 
-  function mer(first, second){
+  function merge(first, second){
     var new_arr=[];
-    console.log("first is "+first+" second is "+second);
-      while (first[0]!=undefined || second[0]!=undefined){
-        console.log("Inside loop: first is "+first+" second is "+second);
-        if (first[0]<second[0] || second[0]==undefined){
-          console.log("first[0] is "+first[0]);
-          console.log("second[0] is "+second[0]);
-          console.log(first[0]<second[0]);
+      while (first.length>0 || second.length>0){
+        if (first[0]<second[0] || second.length==0){
           let x=first.splice(0,1);
           new_arr.push(x);
-          console.log("x new_arr is "+new_arr);
           }
         else {
           let y=second.splice(0,1);
           new_arr.push(y);
-          console.log("y new_arr is "+new_arr);
         }
       }
-    var merged = [].concat.apply([], new_arr);  
-    return merged;
-
+    return merged = [].concat.apply([], new_arr);  
   }
- 
+  
 }
 
 mergeSort([108,15,50,4,8,42,23,16,9,55]);
